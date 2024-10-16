f9036930c4f3b0de0087c1dd926cc284023d73a4
M4GM4 <df27qr15@gmail.com>
2024-10-16 10:14:05 +0200

    Commit initial

---
+++ LogsFinder.py
@@ -0,0 +1,178 @@
+from datetime import datetime
+import glob
+import os
+import re
+
+now = datetime.now()
+
+PATHLOGS = "logs/"
+PATHOUT = "output/"
+
+txtfiles = []
+
+def getDamage(fichiers, player, printmode):
+    damage = []
+    damagecount = 0;
+    player.replace('.','[.]*')
+    stringt = '(((' + player + ').* has taken)|(has taken .*('+ player + ')))|(((' + player + ').* has dealt)|(has dealt .*('+ player + ')))';
+    searchstring = re.compile(stringt);
+    for filename in fichiers:
+        with open(filename, encoding="utf8") as file:
+            for line in file:
+                if searchstring.search(line):
+                    damagecount += 1;
+                    line = line.strip();
+                    damage.append(re.sub('.log', '', re.sub('logs\\\\', '', filename)) + ' | '+ line);
+    if printmode == 0:
+        printLines(damage);
+        print('|--------------------------------------------------------------------------\n' +
+            player + ' a touché / a été touché ' + str(damagecount) + ' fois dans les fichiers log')
+    elif printmode == 1:
+        printFile(damage, player, 'damage');
+    return damagecount;
+
+def getMessages(fichiers, player, printmode):
+    messages = []
+    messagecount = 0;
+    player.replace('.','[.]*')
+    stringt = '('+ player + '.*/PM)|(OOC.*' + player + ')|('+ player + '.*says)|('+ player + '.*/A )';
+    searchstring = re.compile(stringt);
+    for filename in fichiers:
+        with open(filename, encoding="utf8") as file:
+            for line in file:
+                if searchstring.search(line):
+                    messagecount += 1;
+                    line = line.strip();
+                    messages.append(re.sub('.log', '', re.sub('logs\\\\', '', filename)) + ' | '+ line);
+    if printmode == 0:
+        printLines(messages);
+        print('|--------------------------------------------------------------------------\n' +
+            player + ' a envoyé ' + str(messagecount) + ' messages dans les fichiers log')
+    elif printmode == 1:
+        printFile(messages, player, 'messages');
+    return messagecount;
+
+
+def getKills(fichiers, player, printmode):
+    kill = []
+    killcount = 0;
+    player.replace('.','[.]*')
+    stringt = player + ' has dealt .*, killing them!';
+    searchstring = re.compile(stringt);
+    for filename in fichiers:
+        with open(filename, encoding="utf8") as file:
+            for line in file:
+                if searchstring.search(line):
+                    killcount += 1;
+                    line = line.strip();
+                    kill.append(re.sub('.log', '', re.sub('logs\\\\', '', filename)) + ' | '+ line);
+    if printmode == 0:
+        printLines(kill);
+        print('|--------------------------------------------------------------------------\n' +
+            player + ' a tué ' + str(killcount) + ' fois dans les fichiers log')
+    elif printmode == 1:
+        printFile(kill, player, 'kill');
+    return killcount;
+
+def getActivity(fichiers, player, printmode):
+    activity = []
+    for filename in fichiers:
+        with open(filename, encoding="utf8") as file:
+            for line in file:
+                if line.find(player) != -1:
+                    line = line.strip();
+                    activity.append(re.sub('.log', '', re.sub('logs\\\\', '', filename)) + ' | '+ line);
+    if printmode == 0:
+        printLines(activity);
+    elif printmode == 1:
+        printFile(activity, player, 'activite');
+
+def getMorts(fichiers, player, printmode):
+    death = []
+    deathcount = 0;
+    player.replace('.','[.]*')
+    stringt = 'to ' + player + '.*, killing them!';
+    searchstring = re.compile(stringt);
+    for filename in fichiers:
+        with open(filename, encoding="utf8") as file:
+            for line in file:
+                if searchstring.search(line):
+                    deathcount += 1;
+                    line = line.strip();
+                    death.append(re.sub('.log', '', re.sub('logs\\\\', '', filename)) + ' | '+ line);
+    if printmode == 0:
+        printLines(death);
+        print('|--------------------------------------------------------------------------\n' +
+            player + ' est mort ' + str(deathcount) + ' fois dans les fichiers logs')
+    elif printmode == 1:
+        printFile(death, player, 'morts');
+    return deathcount;
+
+def printFile(liste, value, name):
+    fname = PATHOUT + name + "_" + str(value) + "_" + now.strftime("%d-%m-%Y_%Hh-%Mm-%Ss") + ".txt";
+    file = open(fname, "a")
+    for line in liste:
+        file.write(line + '\n');
+    print('|--------------------------------------------------------------------------\n'
+          '|Fichier créé avec succès');
+
+def printLines(liste):
+    print('|--------------------------------------------------------------------------\n')
+    for line in liste:
+        print('|' + line);
+
+def getFiles():
+    for file in glob.glob(PATHLOGS + "*.log"):
+        txtfiles.append(file)
+
+def getStats(fichiers, player, printmode):
+    morts = getDeath(txtfiles, player, 1);
+    kills = getKills(txtfiles, player, 1);
+    KDR = round(kills/morts, 2);
+    print('|--------------------------------------------------------------------------\n'
+          '|Statistiques de ' + player + ' sur les 18 derniers jours:\n'
+          '|--------------------------------------------------------------------------\n'
+          '|Kills : ' + str(kills) + '\t\t Morts : ' + str(morts) + '\t\tKDR : ' + str(KDR)+ '\n'
+          );
+
+def outMode():
+    while 1==1:
+        entryoutputmode = input('Mode de sortie: 0 - Console | 1 - Fichier (dossier /output): ');
+        if re.search('[0-1]', entryoutputmode):
+            outputmode = int(entryoutputmode)
+            return outputmode
+            break;
+        else:
+            print('|--------------------------------------------------------------------------\n' +
+                  '|Entrez une valeur valide');
+
+def main():
+    while 1==1:
+        entry = '';
+        mode = 0;
+        entry = input('|--------------------------------------------------------------------------\n' +
+              '|Choisir le mode de recherche:\n| 1 - Activité | 2 - Mort | 3 - Kill | 4 - Statistiques | 5 - Messages | 6 - Item/Stockage/ID | 7 - Dégats\n');
+        if re.search('[1-7]', entry):
+            mode = int(entry);
+        if mode != 0:
+            getFiles();
+            if mode == 1:
+                getActivity(txtfiles, input("Entrez le nom d'un personnage ou d'un NPC: "), outMode());
+            elif mode == 2:
+                getMorts(txtfiles, input("Entrez le nom d'un personnage ou d'un NPC: "), outMode());
+            elif mode == 3:
+                getKills(txtfiles, input("Entrez le nom d'un personnage ou d'un NPC: "), outMode());
+            elif mode == 4:
+                getStats(txtfiles, input("Entrez le nom d'un personnage ou d'un NPC: "), outMode());
+            elif mode == 6:
+                getActivity(txtfiles, input("Entrez l'identifiant ou le nom d'un item ou d'un stockage: "), outMode());
+            elif mode == 5:
+                getMessages(txtfiles, input("Entrez le nom d'un personnage: "), outMode());
+            elif mode == 7:
+                getDamage(txtfiles, input("Entrez le nom d'un personnage ou d'un NPC: "), outMode());
+        elif mode == 0:
+            print('|--------------------------------------------------------------------------\n' +
+                  '|Entrez une valeur valide');
+
+if __name__ == '__main__':
+    main()
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# -OLRP-LogsFinder
+Voici un petit script python pour vous faciliter la vie lorsque vous cherchez des choses dans les logs sur plusieurs jours.
+
+Ce script necessite d'avoir Python 3 installé pour fonctionner: https://www.python.org/downloads/release/python-3126/
+
+En cas de problèmes ou d'idées d'amélioration, contactez moi sur discord: m4gm4
+
+Fonctionnement:
+
+1. Télecharger les logs que vous voulez verifier
+
+2. Déplaces les dans le dossier "logs/"
+
+3. Double cliquer sur le fichier LogsFinder.py si Python est installé.
+
+4. Une fois le script lancé, choisissez le mode de recherche
+
+5. Entrez ensuite la valeur demandée (nom du joueur, ID de l'item...)
+
+6. Choisissez le mode de sortie des résultats (crée un fichier texte dans le dossier "output/" en mode 1)
+
+7. Si vous avez terminé, fermez la fenêtre
